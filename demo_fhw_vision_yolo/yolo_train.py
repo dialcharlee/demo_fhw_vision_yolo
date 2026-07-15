30f15450d0bca6939702014401289d3ffbffbe65
dialcharlee <143084261+dialcharlee@users.noreply.github.com>
2026-07-15 08:55:32 +0800

    Add files via upload

--- demo_fhw_vision_yolo/yolo_train.py
+++ demo_fhw_vision_yolo/yolo_train.py
@@ -11,16 +11,7 @@ if __name__ == '__main__':
         imgsz=640,
         batch=16,
         device=0,
-        workers=8,          # 如果仍有问题，可暂时降低 workers 或设为 0
+        workers=8,
         lr0=0.01,
-        patience=50,
-        save=True,
-        save_period=10,
         project='runs/train',
-        name='exp',
-        exist_ok=True,
-        pretrained=True,
-        optimizer='auto',
-        seed=42,
-        verbose=True,
     )
